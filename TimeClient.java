1801ff22f5f0c0b316367476978c1f1ec34871ea
kiminha983 <kiminha983@gmail.com>
2023-02-16 15:54:21 +0900

    day24 NIO

--- TimeClient.java
+++ TimeClient.java
@@ -1,33 +1,26 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 public class TimeClient {
-    // MultiCast Client
+    // NIO MultiCast Client
     public static void main(String args[]) {
-        System.out.println("멀티 캐스트 타임 클라이언트");
-        MulticastSocket socket = null;
-        try{
-            socket = new MulticastSocket(10000);
-            InetAddress group = InetAddress.getByName("224.0.0.117");
-            socket.joinGroup(group);
-            System.out.println("멀티캐스트 타임 서버 그룹에 조인 완료");
-            byte[] buffer = new byte[256];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            for (int i = 0; i < 10; i++) {
-                socket.receive(packet);
-                String received = new String(packet.getData());
-                System.out.println(received.trim());
+        System.out.println("NIO 멀티 캐스트 타임 클라이언트");
+        SocketAddress address = new InetSocketAddress("127.0.0.1", 20000);
+        try (SocketChannel sc = SocketChannel.open(address)) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(64); // NON DIRECT
+            int bytesRead =  sc.read(byteBuffer);
+            while (bytesRead > 0) {
+                byteBuffer.flip(); // limit to position, position to zero
+                while (byteBuffer.hasRemaining())
+                    System.out.print((char)byteBuffer.get());
+
+                System.out.println();
+                bytesRead = sc.read(byteBuffer);
             }
-            socket.leaveGroup(group);  // 멀티캐스트 타임 서버 탈퇴
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        catch (IOException ex) {
-            System.out.println("입출력 예외 발생!");
-        }
-        finally {
-            if(socket != null) socket.close();
-        }
-        System.out.println("멀티 캐스트 타임 클라이언트 종료됨");
     }
 }
